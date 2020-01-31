28729288b85234cf28376b9b623495ec7dd7a24a
Douglas Borba <douglas.borba@dbccompany.com.br>
2020-01-31 13:48:35 -0300

    menu atualizar inicio

--- app/src/androidTest/java/com/douglasborba/bollyfilmes/FilmeTest.java
+++ app/src/androidTest/java/com/douglasborba/bollyfilmes/FilmeTest.java
@@ -2,7 +2,14 @@ package com.douglasborba.bollyfilmes;
 
 import junit.framework.TestCase;
 
-public class teste extends TestCase {
+public class FilmeTest extends TestCase {
 
+    public void testFilme(){
+        ItemFilme filme = new ItemFilme("A volta dos que não foram",
+                "O filme dos que não voltaram mas não foram",
+                "85/01/2020",5);
+
+        assertTrue(true);
+    }
 
 }
--- app/src/main/java/com/douglasborba/bollyfilmes/MainActivity.java
+++ app/src/main/java/com/douglasborba/bollyfilmes/MainActivity.java
@@ -1,8 +1,15 @@
 package com.douglasborba.bollyfilmes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView lista = findViewById(R.id.list_filmes);
+
+        ArrayList<ItemFilme> arrayList = new ArrayList<>();
+        arrayList.add(new ItemFilme("Homem aranha 1", "Super herói", "76/01/2020", 3));
+        arrayList.add(new ItemFilme("Homem aranha 2", "Super herói", "76/01/2020", 3.5f));
+        arrayList.add(new ItemFilme("Homem aranha 3", "Super herói", "76/01/2020", 4));
+        arrayList.add(new ItemFilme("Homem aranha 4", "Super herói", "76/01/2020", 1));
+        arrayList.add(new ItemFilme("Homem aranha 5", "Super herói", "76/01/2020", 4.5f));
+
+        FilmesAdapter adapter = new FilmesAdapter(this, arrayList);
+        lista.setAdapter(adapter);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menu_atualizar:
+                Toast.makeText(this, "Atualizando", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
