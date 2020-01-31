38272e8e9eee913eb06fa7e6b034f9378571b793
Douglas Borba <douglas.borba@dbccompany.com.br>
2020-01-31 17:25:08 -0300

    fragment detalhe

--- app/src/main/java/com/douglasborba/bollyfilmes/FilmeDetalheActivity.java
+++ app/src/main/java/com/douglasborba/bollyfilmes/FilmeDetalheActivity.java
@@ -1,6 +1,8 @@
 package com.douglasborba.bollyfilmes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -20,18 +22,15 @@ public class FilmeDetalheActivity extends AppCompatActivity {
         Intent intent = getIntent();
         ItemFilme itemFilme = (ItemFilme) intent.getSerializableExtra(MainActivity.KEY_FILME);
 
-        TextView titulo = findViewById(R.id.item_titulo);
-        titulo.setText(itemFilme.getTitulo());
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-        TextView data = findViewById(R.id.item_data);
-        data.setText(itemFilme.getDataLancamento());
+        FilmeDetalheFragment filmeDetalheFragment = new FilmeDetalheFragment();
+        Bundle bundle = new Bundle();
+        bundle.putSerializable(MainActivity.KEY_FILME, itemFilme);
+        filmeDetalheFragment.setArguments(bundle);
 
-        TextView descricao = findViewById(R.id.item_descricao);
-        descricao.setText(itemFilme.getDescricao());
+        fragmentTransaction.commit();
 
-        RatingBar avaliacao = findViewById(R.id.item_avaliacao);
-        avaliacao.setRating(itemFilme.getAvaliacao());
-
-        Button btn_trailer = findViewById(R.id.item_btn_trailer);
     }
 }
---
+++ app/src/main/java/com/douglasborba/bollyfilmes/FilmeDetalheFragment.java
@@ -0,0 +1,58 @@
+package com.douglasborba.bollyfilmes;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+public class FilmeDetalheFragment extends Fragment {
+
+    private ItemFilme itemFilme;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+            itemFilme = (ItemFilme) getArguments().getSerializable(MainActivity.KEY_FILME);
+        }
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_filme_detalhe, container, false);
+
+        if(itemFilme == null){
+            return view;
+        }
+
+        TextView titulo = view.findViewById(R.id.item_titulo);
+        titulo.setText(itemFilme.getTitulo());
+
+        TextView data = view.findViewById(R.id.item_data);
+        data.setText(itemFilme.getDataLancamento());
+
+        TextView descricao = view.findViewById(R.id.item_descricao);
+        descricao.setText(itemFilme.getDescricao());
+
+        RatingBar avaliacao = view.findViewById(R.id.item_avaliacao);
+        avaliacao.setRating(itemFilme.getAvaliacao());
+
+        Button btn_trailer = view.findViewById(R.id.item_btn_trailer);
+
+        return view;
+    }
+
+}
